6c314a2e8f79ba35f819511289c2eb5e229ec3e4
Repurso <repurso.app@gmail.com>
2026-05-18 01:11:32 +0300

    update sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -34,6 +34,13 @@ export default function sitemap(): MetadataRoute.Sitemap {
     },
 
     {
+      url: `${siteUrl}/carousel-generator`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.9,
+    },
+
+    {
       url: `${siteUrl}/privacy`,
       lastModified: new Date(),
       changeFrequency: "monthly",
