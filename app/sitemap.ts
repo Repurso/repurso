00c6607b4a959da5770592776826510320071d98
Repurso <repurso.app@gmail.com>
2026-05-18 01:05:45 +0300

    update sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -27,6 +27,13 @@ export default function sitemap(): MetadataRoute.Sitemap {
     },
 
     {
+      url: `${siteUrl}/instagram-caption-generator`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.9,
+    },
+
+    {
       url: `${siteUrl}/privacy`,
       lastModified: new Date(),
       changeFrequency: "monthly",
